ff17d3732a0c61bd17fad378959f615b332a389b
h4ckt0zit3 <h4ckt0zit3@users.noreply.github.com>
2026-04-18 20:56:02 +0530

    feat: initialise Hardhat project with Solidity 0.8.20 and OpenZeppelin v5

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+import "@nomicfoundation/hardhat-toolbox";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+export default {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "https://rpc.sepolia.org",
+      accounts: process.env.DEPLOYER_PRIVATE_KEY
+        ? [process.env.DEPLOYER_PRIVATE_KEY]
+        : [],
+    },
+  },
+};
